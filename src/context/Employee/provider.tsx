8691ba2574bae9be417712f6cfb0411683d5ee72
Jonathan Thomaz <jonathanthomaz96@gmail.com>
2023-03-28 22:14:02 -0300

    feat: add start and end time current

--- src/context/Employee/provider.tsx
+++ src/context/Employee/provider.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState } from 'react';
-import { IAxiosResponse, IEmployeeContext } from './interfaces';
+import { IAxiosResponse, ICurrentHours, IEmployeeContext } from './interfaces';
 import instance from 'services';
 import { ITimeRegister } from 'interfaces';
 import { isAxiosError } from 'axios';
@@ -11,6 +11,8 @@ interface IEmployeeProvider {
   children: React.ReactNode;
 }
 export const EmployeeProvider = ({ children }: IEmployeeProvider) => {
+  const [code, setCode] = useState('');
+  const [currentHours, setCurrentHours] = useState<ICurrentHours | null>(null);
   const [beforeDays, setBeforeDays] = useState<ITimeRegister[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
@@ -19,10 +21,66 @@ export const EmployeeProvider = ({ children }: IEmployeeProvider) => {
   const getBeforeDays = async (code: string) => {
     try {
       setIsLoading(true);
-      const response = await instance.get<IAxiosResponse>(
+      const response = await instance.get<IAxiosResponse<ITimeRegister[]>>(
         `/time-register/${code}`
       );
       setBeforeDays(response.data.result);
+      setCode(code);
+      setIsLoading(false);
+    } catch (error) {
+      setIsError(true);
+      if (isAxiosError(error)) {
+        setIsLoading(false);
+        setError(error.message);
+      }
+    }
+  };
+  const getCurrentHours = async (code: string) => {
+    try {
+      setIsLoading(true);
+      const response = await instance.get<IAxiosResponse<ICurrentHours>>(
+        `/time-register/${code}/current`
+      );
+      setCurrentHours(response.data.result);
+      setIsLoading(false);
+    } catch (error) {
+      setIsError(true);
+      if (isAxiosError(error)) {
+        setIsLoading(false);
+        setError(error.message);
+      }
+    }
+  };
+
+  const startTimeRegister = async (code: string) => {
+    try {
+      setIsLoading(true);
+      const response = await instance.post<IAxiosResponse<ICurrentHours>>(
+        `/time-register/${code}/start`,
+        {
+          dateTime: new Date().toISOString()
+        }
+      );
+      setCurrentHours(response.data.result);
+      setIsLoading(false);
+    } catch (error) {
+      setIsError(true);
+      if (isAxiosError(error)) {
+        setIsLoading(false);
+        setError(error.message);
+      }
+    }
+  };
+  const endTimeRegister = async (code: string) => {
+    try {
+      setIsLoading(true);
+      const response = await instance.post<IAxiosResponse<ICurrentHours>>(
+        `/time-register/${code}/end`,
+        {
+          dateTime: new Date().toISOString()
+        }
+      );
+      setCurrentHours(response.data.result);
       setIsLoading(false);
     } catch (error) {
       setIsError(true);
@@ -35,7 +93,18 @@ export const EmployeeProvider = ({ children }: IEmployeeProvider) => {
 
   return (
     <EmployeeContext.Provider
-      value={{ beforeDays, isLoading, isError, error, getBeforeDays }}
+      value={{
+        code,
+        currentHours,
+        getCurrentHours,
+        beforeDays,
+        isLoading,
+        isError,
+        error,
+        getBeforeDays,
+        startTimeRegister,
+        endTimeRegister
+      }}
     >
       {children}
     </EmployeeContext.Provider>
--- src/screens/TimeRegister/index.tsx
+++ src/screens/TimeRegister/index.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, Header, Hours, Strong, User } from './styles';
 import { BeforeDays } from './components';
 import { Button } from 'components';
 import { getTime } from './functions/index';
+import { useEmployee } from 'context';
 
 const TimeRegister = () => {
+  const {
+    code,
+    getCurrentHours,
+    currentHours,
+    startTimeRegister,
+    isLoading,
+    endTimeRegister
+  } = useEmployee();
+
+  useEffect(() => {
+    getCurrentHours(code);
+  }, []);
+
   return (
     <Container>
       <Header>
         <strong>Relógio de ponto</strong>
         <User>
-          <strong>#userCode</strong>
+          <strong>#{code}</strong>
           <span>Usuário</span>
         </User>
       </Header>
       <Hours>
-        <Strong size="24px">{getTime('', '')}</Strong>
+        <Strong size="24px">
+          {isLoading
+            ? 'Carregando...'
+            : getTime(
+                currentHours?.startDateTime ?? '',
+                currentHours?.endDateTime ?? new Date().toISOString()
+              )}
+        </Strong>
         <Strong size="12px">Horas de hoje</Strong>
       </Hours>
-      <Button>{'Hora de entrada'}</Button>
+      {!currentHours?.endDateTime && (
+        <Button
+          onClick={() =>
+            currentHours ? endTimeRegister(code) : startTimeRegister(code)
+          }
+          disabled={isLoading}
+        >
+          {currentHours ? 'Hora de saída' : 'Hora de entrada'}
+        </Button>
+      )}
       <BeforeDays />
     </Container>
   );
